6758676d69793985db2057ecb2d14caa7563fa37
mi-b <mikebaumann@bluewin.ch>
2025-06-01 15:54:17 +0200

    changes from review

--- dot_config/nvim/lua/vim-options.lua
+++ dot_config/nvim/lua/vim-options.lua
@@ -5,3 +5,12 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
+
+
+vim.filetype.add({
+  pattern = {
+    [".*%.yaml%.tmpl"] = "yaml",
+    [".*%.toml%.tmpl"] = "toml",
+    [".*%.sh%.tmpl"] = "sh",
+  },
+})

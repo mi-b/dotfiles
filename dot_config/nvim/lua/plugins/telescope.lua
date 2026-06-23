e87fef995b09a432109f6c9c78926c3c669db947
mi-b <mikebaumann@bluewin.ch>
2026-06-23 10:33:36 +0200

    change telescope find file keymap to <leader>ff

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -13,7 +13,7 @@ return {
 				update_in_insert = false, -- Don't update diagnostics while typing
 				severity_sort = false, -- Sort diagnostics by severity
 			})
-			vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Find Files" })
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live Grep" })
 			vim.keymap.set("n", "<leader>ld", ":Telescope diagnostics<CR>", { desc = "Telescope Diagnostics" })
 		end,

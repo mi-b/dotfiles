e6e210c4e99bcee08869014f87ecca6d641398be
mi-b <mikebaumann@bluewin.ch>
2026-06-25 15:42:07 +0200

    bump telescope

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
 	{
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.8",
+		commit = "427b576",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			local builtin = require("telescope.builtin")

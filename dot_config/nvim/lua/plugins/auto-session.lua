f8baf42da692f0cf5eaa04e26422593313a286ea
mi-b <mikebaumann@bluewin.ch>
2026-06-25 23:54:01 +0200

    enable auto-sessions

--- dot_config/nvim/lua/plugins/auto-session.lua
+++ dot_config/nvim/lua/plugins/auto-session.lua
@@ -3,7 +3,8 @@ return {
   version = "^2.0.0",
   config = function()
     local opts = {
-      auto_restore = false,
+      auto_restore = true,
+      auto_save = true,
     }
     require("auto-session").setup(opts)
   end,

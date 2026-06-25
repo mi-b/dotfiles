7ac3c96dae23730ce2f9326448ffcd80cee57640
mi-b <mikebaumann@bluewin.ch>
2026-06-25 23:07:30 +0200

    switch deprecated loop to uv

--- dot_config/nvim/lua/utils.lua
+++ dot_config/nvim/lua/utils.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 M.is_windows = function()
-  return vim.loop.os_uname().sysname == "Windows_NT"
+  return vim.uv.os_uname().sysname == "Windows_NT"
 end
 
 M.notify_win_skip = function(module_name)

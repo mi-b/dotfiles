202ef3dd04d1203075379bfb958e06a96457daa5
mi-b <mikebaumann@bluewin.ch>
2026-06-25 15:43:47 +0200

    add treesitter-textobjects

---
+++ dot_config/nvim/lua/plugins/treesitter-textobjects.lua
@@ -0,0 +1,38 @@
+return {
+	"nvim-treesitter/nvim-treesitter-textobjects",
+	branch = "main",
+	init = function()
+		vim.g.no_plugin_maps = true
+	end,
+	config = function()
+		require("nvim-treesitter-textobjects").setup({
+			select = {
+				lookahead = true,
+				selection_modes = {
+					["@parameter.outer"] = "v", -- charwise
+					["@function.outer"] = "V", -- linewise
+					-- ['@class.outer'] = '<c-v>', -- blockwise
+				},
+				include_surrounding_whitespace = false,
+			},
+		})
+
+		-- keymaps
+		vim.keymap.set({ "x", "o" }, "am", function()
+			require("nvim-treesitter-textobjects.select").select_textobject("@function.outer", "textobjects")
+		end)
+		vim.keymap.set({ "x", "o" }, "im", function()
+			require("nvim-treesitter-textobjects.select").select_textobject("@function.inner", "textobjects")
+		end)
+		vim.keymap.set({ "x", "o" }, "ac", function()
+			require("nvim-treesitter-textobjects.select").select_textobject("@class.outer", "textobjects")
+		end)
+		vim.keymap.set({ "x", "o" }, "ic", function()
+			require("nvim-treesitter-textobjects.select").select_textobject("@class.inner", "textobjects")
+		end)
+		-- You can also use captures from other query groups like `locals.scm`
+		vim.keymap.set({ "x", "o" }, "as", function()
+			require("nvim-treesitter-textobjects.select").select_textobject("@local.scope", "locals")
+		end)
+	end,
+}

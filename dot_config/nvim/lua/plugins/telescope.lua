7ac30ce3af2f080861019328d0631964b413439a
mi-b <mikebaumann@bluewin.ch>
2026-06-26 00:32:06 +0200

    load telescope frequency properly

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -2,32 +2,30 @@ return {
 	{
 		"nvim-telescope/telescope.nvim",
 		commit = "427b576",
-		dependencies = { "nvim-lua/plenary.nvim" },
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{ "nvim-telescope/telescope-frecency.nvim", version = "^1.0.0" },
+			{ "nvim-telescope/telescope-ui-select.nvim", commit = "6e51d7d" },
+		},
 		config = function()
+			local telescope = require("telescope")
 			local builtin = require("telescope.builtin")
 
-			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files by name" })
-			vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Find recently opened files" })
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Find text in project (grep)" })
-			vim.keymap.set("n", "<leader>lD", builtin.diagnostics, { desc = "List all diagnostics" })
-		end,
-	},
-	{
-		"nvim-telescope/telescope-frecency.nvim",
-		version = "^1.0.0",
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		commit = "6e51d7d",
-		config = function()
-			require("telescope").setup({
+			telescope.setup({
 				extensions = {
 					["ui-select"] = {
 						require("telescope.themes").get_dropdown({}),
 					},
 				},
 			})
-			require("telescope").load_extension("ui-select")
+
+			telescope.load_extension("ui-select")
+			telescope.load_extension("frecency")
+
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files by name" })
+			vim.keymap.set("n", "<leader>fr", "<cmd>Telescope frecency<cr>", { desc = "Find recently opened files (frecency)" })
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Find text in project (grep)" })
+			vim.keymap.set("n", "<leader>lD", builtin.diagnostics, { desc = "List all diagnostics" })
 		end,
 	},
 }

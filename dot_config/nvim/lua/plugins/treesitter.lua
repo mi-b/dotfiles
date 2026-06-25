dfa5064f0024e3ac0386a3fa050b9ff0b58bb41f
mi-b <mikebaumann@bluewin.ch>
2026-06-25 15:43:40 +0200

    format file

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,17 +1,17 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  commit = "4916d6",
-  lazy = false,
-  build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter").setup({
-      install_dir = vim.fn.stdpath("data") .. "/site",
-      ensure_installed = { "lua", "python", "c", "cpp", "bash", "toml", "markdown", "cmake"
-      },
-      auto_install = true,
-      indent = {
-        enable = true,
-      }
-    })
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	commit = "4916d6",
+	lazy = false,
+	build = ":TSUpdate",
+	dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
+	config = function()
+		require("nvim-treesitter").setup({
+			install_dir = vim.fn.stdpath("data") .. "/site",
+			ensure_installed = { "lua", "python", "c", "cpp", "bash", "toml", "markdown", "cmake" },
+			auto_install = true,
+			indent = {
+				enable = true,
+			},
+		})
+	end,
 }

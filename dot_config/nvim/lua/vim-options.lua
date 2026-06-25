3d3a52ab23cc6295669afea496cc84966e68b20a
mi-b <mikebaumann@bluewin.ch>
2026-06-25 23:07:49 +0200

    switch deprecated vim options

--- dot_config/nvim/lua/vim-options.lua
+++ dot_config/nvim/lua/vim-options.lua
@@ -1,9 +1,9 @@
-vim.cmd("set number")
-vim.cmd("set relativenumber")
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.g.mapleader = " "
 
 

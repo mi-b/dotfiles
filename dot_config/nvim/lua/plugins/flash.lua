ffa8d5b24591e7f5b5ed667ff1816d8fb1065a96
mi-b <mikebaumann@bluewin.ch>
2026-06-26 00:50:01 +0200

    replace hop with flash

---
+++ dot_config/nvim/lua/plugins/flash.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/flash.nvim",
+  version = "^2.0.0",
+  event = "VeryLazy",
+  opts = {},
+  keys = {
+    { "s",     function() require("flash").jump() end,              mode = { "n", "x", "o" }, desc = "Jump to any match on screen" },
+    { "S",     function() require("flash").treesitter() end,        mode = { "n", "x", "o" }, desc = "Select treesitter node" },
+    { "r",     function() require("flash").remote() end,            mode = "o",                desc = "Remote flash (operator pending)" },
+    { "R",     function() require("flash").treesitter_search() end, mode = { "o", "x" },       desc = "Search and select treesitter node" },
+    { "<C-s>", function() require("flash").toggle() end,            mode = "c",                desc = "Toggle flash in search mode" },
+  },
+}

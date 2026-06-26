e3d1a8a4cd401e396b2f170e9f342b890638bc6e
mi-b <mikebaumann@bluewin.ch>
2026-06-26 15:31:22 +0200

    ignore .venv in neo-tree

--- dot_config/nvim/lua/plugins/neo-tree.lua
+++ dot_config/nvim/lua/plugins/neo-tree.lua
@@ -12,6 +12,17 @@ return {
 	lazy = false,
 	config = function()
 		require("neo-tree").setup({
+			filesystem = {
+				filtered_items = {
+					hide_dotfiles = false,
+					hide_gitignored = true,
+					hide_by_name = {
+						".venv",
+						"__pycache__",
+						"node_modules",
+					},
+				},
+			},
 			event_handlers = {
 				{
 					event = "file_opened",

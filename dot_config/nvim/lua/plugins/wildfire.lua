56d9699751bfeed46aef08ccb0903435d165538c
mi-b <mikebaumann@bluewin.ch>
2026-06-29 17:17:11 +0200

    add wildfire

---
+++ dot_config/nvim/lua/plugins/wildfire.lua
@@ -0,0 +1,16 @@
+-- wildfire.nvim: Incremental treesitter node selection
+-- https://github.com/SUSTech-data/wildfire.nvim
+return {
+	"sustech-data/wildfire.nvim",
+	event = "VeryLazy",
+	dependencies = { "nvim-treesitter/nvim-treesitter" },
+	config = function()
+		require("wildfire").setup({
+			keymaps = {
+				init_selection = "<CR>",
+				node_incremental = "<CR>",
+				node_decremental = "<BS>",
+			},
+		})
+	end,
+}

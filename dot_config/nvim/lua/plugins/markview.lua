6ca08d2dda3fefcaae91e6b8dcde4908fbea35a6
mi-b <mikebaumann@bluewin.ch>
2026-06-29 14:44:29 +0200

    add markview

---
+++ dot_config/nvim/lua/plugins/markview.lua
@@ -0,0 +1,17 @@
+if require("utils").skip_if_windows("markview") then
+	return {}
+end
+
+return {
+	"OXY2DEV/markview.nvim",
+	ft = { "markdown", "typst", "latex", "html", "yaml" },
+
+	config = function()
+		require("markview").setup({
+			preview = { enable = false },
+		})
+
+		vim.api.nvim_set_keymap("n", "<leader>MM", "<CMD>Markview toggle<CR>", { desc = "markview: Toggle preview (buffer)" })
+		vim.api.nvim_set_keymap("n", "<leader>MS", "<CMD>Markview splitToggle<CR>", { desc = "markview: Toggle splitview" })
+	end,
+}

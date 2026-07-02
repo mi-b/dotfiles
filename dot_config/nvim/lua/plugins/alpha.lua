1693bdc955b0e0494c4f156f321d3351e717614d
mi-b <mikebaumann@bluewin.ch>
2026-07-02 10:30:43 +0200

    add flying dragon to dashboard

--- dot_config/nvim/lua/plugins/alpha.lua
+++ dot_config/nvim/lua/plugins/alpha.lua
@@ -1,79 +1,90 @@
 -- alpha-nvim: Customisable start screen with quick-access buttons
 -- https://github.com/goolord/alpha-nvim
 return {
-  "goolord/alpha-nvim",
-  commit = "6c6a89d",
-  dependencies = { { "rubiin/fortune.nvim", version = "^1.0.0" } },
-  config = function()
-    local alpha = require("alpha")
-    local dashboard = require("alpha.themes.dashboard")
+	"goolord/alpha-nvim",
+	commit = "6c6a89d",
+	dependencies = {
+		{ "rubiin/fortune.nvim", version = "^1.0.0" },
+		"amansingh-afk/milli.nvim",
+	},
+	config = function()
+		local alpha = require("alpha")
+		local dashboard = require("alpha.themes.dashboard")
 
-    -- Set header
-    -- check out https://patorjk.com/software/taag/#p=testall&v=0&f=1Row&t=NEOVIM
-    -- dashboard.section.header.val = {
-    --   "                                                     ",
-    --   "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-    --   "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-    --   "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-    --   "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-    --   "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-    --   "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-    --   "                                                     ",
-    -- }
+    -- Set header to first frame of milli animation (milli replaces it with
+    -- subsequent frames once the alpha buffer opens)
+    -- Crop the splash to remove blank padding rows (45 -> ~23 lines)
+    local ok, splash = pcall(require, "milli.splashes.flyingdragon")
+    if ok and splash.frames then
+      local top, bottom = 13, 36 -- keep rows 14-36 (content region)
+      for i, frame in ipairs(splash.frames) do
+        splash.frames[i] = vim.list_slice(frame, top + 1, bottom)
+      end
+      if splash.colors then
+        for i, cframe in ipairs(splash.colors) do
+          splash.colors[i] = vim.list_slice(cframe, top + 1, bottom)
+        end
+      end
+      for i, d in ipairs(splash.delays) do
+        splash.delays[i] = d * 1.1
+      end
+      splash.rows = bottom - top
+      dashboard.section.header.val = splash.frames[1]
+    else
+			dashboard.section.header.val = {
+				[[                                                                       ]],
+				[[                                                                       ]],
+				[[                                                                       ]],
+				[[                                                                       ]],
+				[[                                                                     ]],
+				[[       ████ ██████           █████      ██                     ]],
+				[[      ███████████             █████                             ]],
+				[[      █████████ ███████████████████ ███   ███████████   ]],
+				[[     █████████  ███    █████████████ █████ ██████████████   ]],
+				[[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+				[[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+				[[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+				[[                                                                       ]],
+				[[                                                                       ]],
+				[[                                                                       ]],
+			}
+		end
 
-    dashboard.section.header.val = {
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                     ]],
-      [[       ████ ██████           █████      ██                     ]],
-      [[      ███████████             █████                             ]],
-      [[      █████████ ███████████████████ ███   ███████████   ]],
-      [[     █████████  ███    █████████████ █████ ██████████████   ]],
-      [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
-      [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
-      [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-    }
+		-- Set menu
+		dashboard.section.buttons.val = {
+			dashboard.button("e", "  New file", ":enew <BAR> startinsert <CR>"),
+			dashboard.button("f", "󰈞  Find file", ":Telescope find_files<CR>"),
+			dashboard.button("r", "  Recent", ":Telescope oldfiles<CR>"),
+			dashboard.button("l", "󰁯  Last session", ":SessionRestore<CR>"),
+			dashboard.button("m", "󰙅  Most frequent", ":Telescope frecency workspace=CWD<CR>"),
+			dashboard.button("s", "  Settings", ":e $MYVIMRC | :cd %:p:h | :Neotree<CR>"),
+			dashboard.button("q", "⏻  Quit NVIM", ":qa<CR>"),
+		}
 
-    -- Set menu
-    dashboard.section.buttons.val = {
-      dashboard.button("e", "  New file", ":enew <BAR> startinsert <CR>"),
-      dashboard.button("f", "󰈞  Find file", ":Telescope find_files<CR>"),
-      dashboard.button("r", "  Recent", ":Telescope oldfiles<CR>"),
-      dashboard.button("l", "󰁯  Last session", ":SessionRestore<CR>"),
-      dashboard.button("m", "󰙅  Most frequent", ":Telescope frecency workspace=CWD<CR>"),
-      dashboard.button("s", "  Settings", ":e $MYVIMRC | :cd %:p:h | :Neotree<CR>"),
-      dashboard.button("q", "⏻  Quit NVIM", ":qa<CR>"),
-    }
+		local footer = function()
+			local info = {}
+			-- info[1] = "  Neovim loaded " .. vim.fn.strftime("%H:%M") .. " on " .. vim.fn.strftime("%d/%m/%Y") .. " '"
+			info[1] = ""
+			local fortune = require("fortune")
+			fortune.setup({
+				display_format = "mixed",
+				content_types = "mixed",
+			})
+			local footer = vim.list_extend(info, fortune.get_fortune())
+			return footer
+		end
 
-    local footer = function()
-      local info = {}
-      -- info[1] = "  Neovim loaded " .. vim.fn.strftime("%H:%M") .. " on " .. vim.fn.strftime("%d/%m/%Y") .. " '"
-      info[1] = ""
-      local fortune = require("fortune")
-      fortune.setup({
-        display_format = "mixed",
-        content_types = "mixed",
-      })
-      local footer = vim.list_extend(info, fortune.get_fortune())
-      return footer
-    end
+		dashboard.section.footer.val = footer()
 
-    dashboard.section.footer.val = footer()
+		-- Send config to alpha
+		alpha.setup(dashboard.opts)
 
-    -- Send config to alpha
-    alpha.setup(dashboard.opts)
-
-    -- Disable folding on alpha buffer
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = "alpha",
-      callback = function()
-        vim.opt_local.foldenable = false
-      end,
-    })
-  end,
+		-- Disable folding on alpha buffer
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "alpha",
+			callback = function()
+				vim.opt_local.foldenable = false
+			end,
+		})
+	end,
 }

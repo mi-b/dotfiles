e127cd01864836dd25e04d3d3e641133465fe661
mi-b <mikebaumann@bluewin.ch>
2026-07-02 13:16:40 +0200

    skip building nvim-treesitter on windows

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,33 +1,37 @@
 -- nvim-treesitter: Syntax-aware highlighting, indentation, and code parsing
 -- https://github.com/nvim-treesitter/nvim-treesitter
+local is_windows = vim.uv.os_uname().sysname == "Windows_NT"
 return {
 	"nvim-treesitter/nvim-treesitter",
 	branch = "main",
 	version = false,
 	lazy = false,
-	build = ":TSUpdate",
+	build = not is_windows and ":TSUpdate" or nil,
 	config = function()
 		require("nvim-treesitter").setup({
 			install_dir = vim.fn.stdpath("data") .. "/site",
 		})
 
 		-- Install parsers (async, no-op if already installed)
-		require("nvim-treesitter").install({
-			"lua",
-			"python",
-			"c",
-			"cpp",
-			"bash",
-			"toml",
-			"markdown",
-			"markdown_inline",
-			"cmake",
-			"html",
-			"latex",
-			"typst",
-			"yaml",
-			"comment",
-		})
+		-- Skipped on Windows where no C compiler is available
+		if not is_windows then
+			require("nvim-treesitter").install({
+				"lua",
+				"python",
+				"c",
+				"cpp",
+				"bash",
+				"toml",
+				"markdown",
+				"markdown_inline",
+				"cmake",
+				"html",
+				"latex",
+				"typst",
+				"yaml",
+				"comment",
+			})
+		end
 
 		-- Enable treesitter-based indentation for all filetypes with a parser
 		vim.api.nvim_create_autocmd("FileType", {

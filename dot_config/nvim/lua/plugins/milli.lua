4706514bcd3d37a480e3aa0dace5f77ca81c4739
mi-b <mikebaumann@bluewin.ch>
2026-07-02 10:23:19 +0200

    add milli for animations (will be used in alpha dashboard)

---
+++ dot_config/nvim/lua/plugins/milli.lua
@@ -0,0 +1,9 @@
+-- milli.nvim: Animated splash screen for the alpha dashboard
+-- https://github.com/amansingh-afk/milli.nvim
+return {
+	"amansingh-afk/milli.nvim",
+	lazy = false,
+	config = function()
+		require("milli").alpha({ splash = "flyingdragon", loop = true })
+	end,
+}

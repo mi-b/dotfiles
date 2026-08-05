7f2bdd4c59647072e685f92723377891d88b23cb
mi-b <mikebaumann@bluewin.ch>
2026-08-05 10:06:00 +0200

    add screenkey

---
+++ dot_config/nvim/lua/plugins/screenkey.lua
@@ -0,0 +1,8 @@
+-- screenkey.nvim: Display pressed keys on screen
+-- https://github.com/NStefan002/screenkey.nvim
+-- Commands: :Screenkey to toggle
+return {
+  "NStefan002/screenkey.nvim",
+  lazy = false,
+  version = "*",
+}

967866b081850cb32ebbf29af67dd223f264d3f2
mi-b <mikebaumann@bluewin.ch>
2026-08-06 22:40:43 +0200

    make inactive windows slightly transparent

--- dot_config/hypr/configs/appearance.lua
+++ dot_config/hypr/configs/appearance.lua
@@ -34,7 +34,7 @@ hl.config({
         rounding = 8,
         rounding_power = 2,
         active_opacity = 1.0,
-        inactive_opacity = 1.0,
+        inactive_opacity = 0.85,
 
         shadow = {
             enabled = true,

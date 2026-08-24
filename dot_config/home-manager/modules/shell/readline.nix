84a6d7bf818f5a54268836f50a7bb82e2cc6df76
mi-b <mikebaumann@bluewin.ch>
2026-08-24 20:35:32 +0200

    simplify bash and tmux and readline config

--- dot_config/home-manager/modules/shell/readline.nix
+++ dot_config/home-manager/modules/shell/readline.nix
@@ -4,9 +4,13 @@
   programs.readline = {
     enable = true;
     variables = {
+      editing-mode = "vi";
       keyseq-timeout = 50;
       completion-ignore-case = true;
       show-all-if-ambiguous = true;
+      show-mode-in-prompt = true;
+      vi-ins-mode-string = "\\1\\e[5 q\\2";
+      vi-cmd-mode-string = "\\1\\e[1 q\\2";
     };
     extraConfig = ''
       set keymap vi-command

86ef1b204d8c01874ab12dcfe0fb6ae7e4100b6b
mi-b <mikebaumann@bluewin.ch>
2026-08-24 21:32:11 +0200

    feat(tmux): add terminal, escapeTime, historyLimit, baseIndex, mouse
    Set tmux-256color for proper truecolour support. Reduce escape time
    to 10ms to eliminate vi-mode lag. Increase scrollback to 50000 lines.
    Start window numbering at 1. Enable mouse support.

--- dot_config/home-manager/modules/terminal/tmux.nix
+++ dot_config/home-manager/modules/terminal/tmux.nix
@@ -3,6 +3,11 @@
 {
   programs.tmux = {
     enable = true;
+    terminal = "tmux-256color";
+    escapeTime = 10;
+    historyLimit = 50000;
+    baseIndex = 1;
+    mouse = true;
     keyMode = "vi";
     customPaneNavigationAndResize = true;
     extraConfig = ''

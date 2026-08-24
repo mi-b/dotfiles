acef2aed37e00a1c5591e9e5d58c642634224f2c
mi-b <mikebaumann@bluewin.ch>
2026-08-24 20:26:02 +0200

    refactor(yazi): use programs.yazi.flavors for Catppuccin theme
    Replace xdg.configFile entry with programs.yazi.flavors option.
    The fetchFromGitHub source is unchanged; a runCommand derivation
    creates the expected directory structure for the flavour.

--- dot_config/home-manager/modules/tools/yazi.nix
+++ dot_config/home-manager/modules/tools/yazi.nix
@@ -1,10 +1,26 @@
 { pkgs, ... }:
 
+let
+  catppuccin-yazi = pkgs.fetchFromGitHub {
+    owner = "catppuccin";
+    repo = "yazi";
+    rev = "d62802be39210ea10e54b3e3b09735c6cb9e57c1";
+    hash = "sha256-bwzEO8exoBwa19q+jnYjHkaamGl2mhfukIEhDfUCRGI=";
+  };
+
+  catppuccin-mocha-flavor = pkgs.runCommand "catppuccin-mocha-yazi" { } ''
+    mkdir -p $out
+    cp ${catppuccin-yazi}/themes/mocha/catppuccin-mocha-blue.toml $out/flavor.toml
+  '';
+in
 {
   programs.yazi = {
     enable = true;
     enableBashIntegration = true;
     shellWrapperName = "y";
+
+    flavors.catppuccin-mocha = catppuccin-mocha-flavor;
+
     settings = {
       mgr = {
         show_hidden = true;
@@ -16,19 +32,11 @@
         max_height = 1080;
       };
     };
+
     theme = {
       flavor = {
         dark = "catppuccin-mocha";
       };
     };
   };
-
-  xdg.configFile."yazi/flavors/catppuccin-mocha.yazi/flavor.toml".source = let
-    catppuccin-yazi = pkgs.fetchFromGitHub {
-      owner = "catppuccin";
-      repo = "yazi";
-      rev = "d62802be39210ea10e54b3e3b09735c6cb9e57c1";
-      hash = "sha256-bwzEO8exoBwa19q+jnYjHkaamGl2mhfukIEhDfUCRGI=";
-    };
-  in "${catppuccin-yazi}/themes/mocha/catppuccin-mocha-blue.toml";
 }

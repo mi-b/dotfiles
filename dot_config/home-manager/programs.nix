10c7fef690a0d11fb3cdb5d955abd3fa2fbcbdbe
mi-b <mikebaumann@bluewin.ch>
2026-08-24 14:17:24 +0200

    refactor(readline): migrate inputrc to home-manager

--- dot_config/home-manager/programs.nix
+++ dot_config/home-manager/programs.nix
@@ -276,4 +276,20 @@
       set -g allow-passthrough on
     '';
   };
+
+  # --- readline ---
+  programs.readline = {
+    enable = true;
+    variables = {
+      keyseq-timeout = 50;
+      completion-ignore-case = true;
+      show-all-if-ambiguous = true;
+    };
+    extraConfig = ''
+      set keymap vi-command
+      "\C-l": clear-screen
+      set keymap vi-insert
+      "\C-l": clear-screen
+    '';
+  };
 }

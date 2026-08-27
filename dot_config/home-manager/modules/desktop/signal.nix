1ca729e00e37f369fce2dbc2eae24c6f2ed02cdf
mib <michael.baumann@helbling.ch>
2026-08-27 15:13:31 +0200

    fix(desktop): remove --no-sandbox flag from signal-desktop
    The AppArmor profile for Nix store binaries now permits unprivileged
    user namespaces, so Electron's sandbox works without the flag.

--- dot_config/home-manager/modules/desktop/signal.nix
+++ dot_config/home-manager/modules/desktop/signal.nix
@@ -9,16 +9,6 @@
   options.host.signalDesktop.enable = lib.mkEnableOption "Signal Desktop";
 
   config = lib.mkIf config.host.signalDesktop.enable {
-    home.packages = [
-      (pkgs.symlinkJoin {
-        name = "signal-desktop-wrapped";
-        paths = [ pkgs.signal-desktop ];
-        nativeBuildInputs = [ pkgs.makeWrapper ];
-        postBuild = ''
-          wrapProgram $out/bin/signal-desktop \
-            --add-flags "--no-sandbox"
-        '';
-      })
-    ];
+    home.packages = [ pkgs.signal-desktop ];
   };
 }

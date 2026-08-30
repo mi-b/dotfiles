439254ffa8671eab8bffe958da75fba8d10840a1
mi-b <mikebaumann@bluewin.ch>
2026-08-30 11:22:32 +0200

    fix hyprland sleep wakeup issues

--- dot_config/home-manager/modules/desktop/hyprland/hypridle.nix
+++ dot_config/home-manager/modules/desktop/hyprland/hypridle.nix
@@ -9,7 +9,7 @@
       general = {
         lock_cmd = "pidof hyprlock || hyprlock";
         before_sleep_cmd = "loginctl lock-session";
-        after_sleep_cmd = "sleep 1 && hyprctl dispatch 'hl.dsp.dpms(\"on\")'"; 
+        after_sleep_cmd = "sleep 1 && hyprctl dispatch dpms on";
       };
 
       listener = [
@@ -27,8 +27,8 @@
         # Turn off display after 15 minutes
         {
           timeout = 900;
-          on-timeout = "hyprctl dispatch 'hl.dsp.dpms(\"off\")'";
-          on-resume = "hyprctl dispatch 'hl.dsp.dpms(\"on\")'"; 
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpms on";
         }
       ];
     };

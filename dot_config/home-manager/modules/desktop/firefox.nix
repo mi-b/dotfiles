1f0b9a1ad6a849188206746155f2c8e80088591f
mi-b <mikebaumann@bluewin.ch>
2026-09-03 12:01:45 +0200

    prevent hypridle when firefox is running

--- dot_config/home-manager/modules/desktop/firefox.nix
+++ dot_config/home-manager/modules/desktop/firefox.nix
@@ -1,6 +1,13 @@
 { config, pkgs, ... }:
 
 {
+  home.sessionVariables = {
+    # Firefox uses D-Bus idle inhibition under Hypridle; force native Wayland
+    # and portal integration so media playback is visible to the session.
+    MOZ_ENABLE_WAYLAND = "1";
+    GTK_USE_PORTAL = "1";
+  };
+
   programs.firefox = {
     enable = true;
     # nixGL wrapper required on non-NixOS to expose host GL/EGL drivers.

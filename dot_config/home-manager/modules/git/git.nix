b1e9739cefddae38ad91f1efa5fc1fbca1bd5e6c
mib <michael.baumann@helbling.ch>
2026-09-18 16:25:59 +0200

    update git config

--- dot_config/home-manager/modules/git/git.nix
+++ dot_config/home-manager/modules/git/git.nix
@@ -8,6 +8,15 @@
       user.name = config.host.gitUserName;
       user.email = config.host.gitUserEmail;
       alias.pushf = "push --force-with-lease";
+      alias.s = "status --short --branch";
+      alias.l = "log --oneline --decorate --graph -20";
+      alias.ll = "log --oneline --decorate --graph --all";
+      alias.last = "log -1 HEAD --stat";
+      alias.unstage = "restore --staged";
+      alias.discard = "restore";
+      alias.amend = "commit --amend --no-edit";
+      alias.branches = "branch --sort=-committerdate";
+      alias.recent = "for-each-ref --sort=-committerdate refs/heads/ --format='%(refname:short) %(committerdate:relative)'";
       core.autocrlf = "input";
       init.defaultBranch = "main";
       column.ui = "auto";
@@ -15,9 +24,12 @@
       pull.rebase = true;
       merge.conflictStyle = "zdiff3";
       rerere.enabled = true;
+      rerere.autoUpdate = true;
       diff.algorithm = "histogram";
       diff.colorMoved = "default";
       fetch.prune = true;
+      push.autoSetupRemote = true;
+      push.useForceIfIncludes = true;
       push.recurseSubmodules = "check";
       submodule.recurse = true;
       transfer.fsckObjects = true;
